4b0b5d932242145f3dac31130139faabb557ee2a
zhang759740844 <759740844@qq.com>
2019-10-14 20:18:38 +0800

    add 字数统计

---
+++ source/count.js
@@ -0,0 +1,47 @@
+var fs = require('fs')
+var path = require('path')// 解析需要遍历的文件夹
+var filePath = path.resolve('./_posts')
+// 调用文件遍历方法
+fileDisplay(filePath)
+let length = 0
+// 文件遍历方法
+function fileDisplay (filePath) {
+  // 根据文件路径读取文件，返回文件列表
+  fs.readdir(filePath, function (err, files) {
+    if (err) {
+      console.warn(err)
+    } else {
+      // 遍历读取到的文件列表
+      files.forEach(function (filename) {
+        // 获取当前文件的绝对路径
+        var filedir = path.join(filePath, filename)
+        // 根据文件路径获取文件信息，返回一个fs.Stats对象
+        fs.stat(filedir, function (eror, stats) {
+          if (eror) {
+            console.warn('获取文件stats失败')
+          } else {
+            var isFile = stats.isFile()// 是文件
+            var isDir = stats.isDirectory()// 是文件夹
+            if (isFile) {
+              console.log('当前读取的文件是： ' + filedir)
+              // 读取文件内容
+              var content = fs.readFileSync(filedir, 'utf-8')
+              for (var i = 0; i < content.length; i++) {
+                if (content.charCodeAt(i) > 127) {
+                  length++
+                }
+              }
+            }
+            if (isDir) {
+              fileDisplay(filedir)// 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+            }
+          }
+        })
+      })
+    }
+  })
+}
+
+setTimeout(() => {
+  console.log('最终的长度是: ' + length)
+}, 2000)
